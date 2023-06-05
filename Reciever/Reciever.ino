4e5420cb879a3fca9ff1bbaf630cf4ea32a4a13b
s-orug <orugants@email.sc.edu>
2023-06-05 14:19:54 -0400

    test code for the transmission

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,26 +1,25 @@
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(9, 10);
 
-const byte address[6] = "00001";
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 void setup() {
   Serial.begin(9600);
+
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.openReadingPipe(1, pipe);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.startListening();
-  pinMode(2, OUTPUT);
 }
 
 void loop() {
   if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    digitalWrite(2, HIGH);
-  }
-    digitalWrite(2, LOW);
+    char receivedData[256]; 
+    radio.read(&receivedData, sizeof(receivedData));
 
+    Serial.print("Received Data: ");
+    Serial.println(receivedData);
+  }
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,19 +1,54 @@
 #include <SPI.h>
-#include <nRF24L01.h>
+#include <SD.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+File dataFile;
+const int chipSelect = 10;
+RF24 radio(9, 10);
 
-const byte address[6] = "00001";
+const uint64_t pipe = 0xE8E8F0F0E1LL; // pipe address
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
+
+  if (!SD.begin(chipSelect))
+  {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+
   radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.openWritingPipe(pipe);
+  radio.setPALevel(RF24_PA_HIGH);
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+void loop()
+{
+  dataFile = SD.open("data.csv");
+  if (dataFile)
+  {
+    while (dataFile.available())
+    {
+      String line = dataFile.readStringUntil('\n');
+      line.trim();
+
+      radio.write(line.c_str(), line.length());
+
+      Serial.print("Transmitted Data: ");
+      Serial.println(line);
+
+      delay(1000);
+    }
+
+    dataFile.close();
+    Serial.println("*******Completed the Transmission*******")
+    while (true)
+    {
+    }
+  }
+  else
+  {
+    Serial.println("Error opening data.csv");
+  }
 }
